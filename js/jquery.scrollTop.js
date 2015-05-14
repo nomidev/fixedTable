421ae59ce4755165db2e8c20610caf7a20c26dfb
nomi <nomisoul@gmail.com>
2015-05-14 18:14:00 +0900

    플로그인 생성

--- js/jquery.scrollTop.js
+++ js/jquery.scrollTop.js
@@ -6,6 +6,49 @@
  * @author: nomi, nomisoul@gmail.com
  **/
 
+(function ($) {
+	$.fn.extend({
+		scrollUp: function (option) {
+			var defaults = {
+				titleClass: "subTitle",
+				copyClass: "subCopy"
+			};
+			var options = $.extend({}, defaults, option);
+
+			return this.each(function () {
+				var textArray = [];
+				var text = $('.' + options.titleClass).text();
+
+				$('.' + options.copyClass).hide();
+
+				for (var i=0,len = text.length; i < len ;i++ ){
+					textArray.push( $('<span>' + text[ i ] + '</span>' ) );
+				}
+
+				$('.' + options.titleClass).text( '' );
+
+				$.each( textArray , function( i, el ){
+					$('.' + options.titleClass).append( el );
+					el.css({
+						'position' : 'relative',
+						'top' : -20,
+						'opacity' : 0
+					});
+					setTimeout( function(){
+						el.animate({
+							'top' : 0,
+							'opacity' : 1
+						} , 200);
+					}, 100 * i );
+				});
+				setTimeout( function(){
+					$('.' + options.copyClass).fadeIn( 500 );
+				} , 100 * (text.length + 1) );
+			});
+		}
+	});
+})(jQuery);
+
 $(document).ready(function () {
 	$(window).scroll(function () {
 		var top = $(window).scrollTop();
